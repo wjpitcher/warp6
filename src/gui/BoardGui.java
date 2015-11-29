e6dd58b41d1addc249c8dba8ea8e4120a9a5b607
wjpitcher <wjpitcher@gmail.com>
2015-11-28 20:11:42 -0600

    BoardGui EventHandler
    Added Mouse and click handler.
    Added test code.

--- src/gui/BoardGui.java
+++ src/gui/BoardGui.java
@@ -1,55 +1,72 @@
 package gui;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-public class BoardGui extends JPanel implements ActionListener {
+@SuppressWarnings("serial")
+public class BoardGui extends JPanel 
+implements MouseListener, MouseMotionListener {
+	/*
 	private int _boardHeight = -1;
 	private int _boardWidth = -1;
 	private int _boardX = -1;
 	private int _boardY = -1;
+	*/
 	private int _panelHeight = -1;
 	private int _panelWidth = -1;
 	BufferedImage imgBoard;
 	
+	private NodesGui _nodes = new NodesGui();			
+			
 	public BoardGui(){
 		//_Board = new Board();
-		
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
 		try {
 			imgBoard = ImageIO.read(new File("img/Warp6.png"));
+			setPreferredSize(new Dimension(imgBoard.getWidth(), imgBoard.getHeight()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+	/*
+	public int getboardHeight() { return _boardHeight; }
+	public int getboardWidth() { return _boardWidth; }
+	public int getboardX() { return _boardX; }
+	public int getboardY() { return _boardY; }
+	*/
 	@Override
 	public void paint(Graphics g){
 		super.paint(g);
+
 		ResizeBoard();
-	
-		g.drawImage(imgBoard, _boardX, _boardY, _boardWidth, _boardHeight ,null);
+		
+		//g.drawImage(imgBoard, _boardX, _boardY, _boardWidth, _boardHeight, null);
+		g.drawImage(imgBoard, 0, 0
+				  , (int)getSize().getWidth()
+				  , (int)getSize().getHeight(), null);
+		_nodes.draw(g, 0, 0);
 	}
-	
+	 
 	private void ResizeBoard(){
 
 		// Check verify the dimensions changed 
-		if(_panelHeight != this.getSize().height
-		|| _panelWidth  != this.getSize().width)
+		if(_panelHeight != getSize().height
+		|| _panelWidth  != getSize().width)
 		{		
-			_panelHeight = this.getSize().height;
-			_panelWidth  = this.getSize().width;
-
+			_panelHeight = getSize().height;
+			_panelWidth  = getSize().width;
+/*
 			// determine the size of the board
 			if(imgBoard.getWidth() > imgBoard.getHeight()){
 				if (_panelWidth / _panelHeight
@@ -83,14 +100,42 @@ public class BoardGui extends JPanel implements ActionListener {
 						_boardX = (_panelWidth - _boardWidth) / 2;
 					}
 			}
+	*/		
+			_nodes.setSize(getSize());
+			NodeGui.radius = (int)(.028 * _panelWidth);
+			
+		//	this.setPreferredSize(new Dimension(_boardX, _boardY));
 		}
-		
-		
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		_nodes.Add(new NodeGui(e.getX(), e.getY(), getWidth(), getHeight()));
+		_nodes.draw(this.getGraphics(), e.getX(), e.getY());
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mousePressed(MouseEvent e) {
+		// Start Node Draw
 	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// Save node
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {/* Not Used */}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+	//	System.out.println("(" + e.getX() + ", " + e.getY() + ")");
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)  { /* Not Used */ }
+
+	@Override
+	public void mouseExited(MouseEvent e) { /* Not Used */ }
 }
